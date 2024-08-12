3b5759f9a604426f04fd375fafc0fdcb040cd56c
Oleksandr Kushchyk <oleksandr.kushchyk@nixs.com>
2024-08-12 15:54:26 +0300

    Added scss modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import classes from './App.module.scss'
 import './App.css'
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className={classes.container}>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
